656aeef5c66fd6de818e0f70dd2559d873b619c9
muthuism <muthuism.09@gmail.com>
2021-09-01 21:14:37 +0530

    easy eureka server

---
+++ easyeurekaserver/src/test/java/com/easyeurekaserver/EasyeurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easyeurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyeurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
